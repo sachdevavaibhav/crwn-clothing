387bd133856fb2255a1d3bef1e8ba3df4d5d755f
sachdevavaibhav <sachdevavaibhav.2001@gmail.com>
2023-05-22 16:27:45 +0530

    shop data from firestore, category preview compenents and routes, category specific route

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,24 @@
+import { createContext, useState, useEffect } from "react"
+
+import {getCategoriesAndDocumnets} from '../utils/firebase/firebase.utils.js'
+
+// import SHOP_DATA from '../shop-data.js'
+
+export const CategoriesContext = createContext({
+    categoriesMap: {}
+})
+
+export const CategoriesProvider = ({ children }) => {
+    const [categoriesMap, setCategoriesMap] = useState({})
+    useEffect(() => {
+        const getCategoryMap = async () => {
+            const categoryMap = await getCategoriesAndDocumnets()
+            console.log(categoryMap)
+            setCategoriesMap(categoryMap)
+        }
+        getCategoryMap() 
+    }, [])
+    const value = {  categoriesMap }
+    return <CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>
+}
+

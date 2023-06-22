168f49a0cd2f6e002ee51bb6ec6d70f9f4adc44d
sachdevavaibhav <sachdevavaibhav.2001@gmail.com>
2023-06-22 23:11:37 +0530

    added redux persist, moved loggerMiddleware to separate file

--- src/store/store.js
+++ src/store/store.js
@@ -1,23 +1,21 @@
 import {compose, legacy_createStore as createStore, applyMiddleware} from 'redux';
+import {persistStore, persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import {logger} from 'redux-logger';
-import { rootReducer } from './root-reducer';
+import {rootReducer} from './root-reducer'
 
-const loggerMiddleware = (store) => (next) => (action) => {
-    if (!action.type) {
-        return next(action)
-    }
-
-    console.log('type: ', action.type)
-    console.log('payload: ', action.payload)
-    console.log('current state: ', store.getState())
-
-    next(action)
-
-    console.log('next state: ', store.getState())
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
 }
 
-const middlewares = [loggerMiddleware];
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
 
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 
-export const store = createStore(rootReducer,undefined,composedEnhancers)
+export const store = createStore(persistedReducer,undefined,composedEnhancers)
+
+export const persistor = persistStore(store)

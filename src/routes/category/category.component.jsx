46d5082a89ff94a81f3cd2c0a1baf020b63595ce
sachdevavaibhav <sachdevavaibhav.2001@gmail.com>
2023-06-23 18:05:02 +0530

    added category fetch login in thunk middleware, spinner component

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,13 +2,15 @@ import { useParams } from "react-router-dom"
 import {CategoryContainer, CategoryTitle} from './category.styles'
 import { useState, useEffect, Fragment } from "react"
 import { useSelector } from "react-redux"
-import { selectCategoriesMap } from "../../store/categories/categories.selector"
+import Spinner from "../../components/spinner/spinner.component"
+import { selectCategoriesMap, selectCategoriesIsLoading } from "../../store/categories/categories.selector"
 import ProductsCard from "../../components/products-card/products-card.component"
 
 const Category = () => {
     const { category } = useParams()
     console.log('render/re-rendering category component', category)
     const categoriesMap = useSelector(selectCategoriesMap)
+    const isLoading = useSelector(selectCategoriesIsLoading)
     const [products, setProducts] = useState(categoriesMap[category])
 
     useEffect(() => {
@@ -18,11 +20,13 @@ const Category = () => {
     return (
         <Fragment>
             <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
-            <CategoryContainer>
-                {products && products.map((product) => (
-                    <ProductsCard key={product.id} product={product} />
-                ))}
-            </CategoryContainer>
+            {isLoading ? <Spinner /> : 
+                <CategoryContainer>
+                    {products && products.map((product) => (
+                        <ProductsCard key={product.id} product={product} />
+                    ))}
+                </CategoryContainer>
+            }
         </Fragment>
     )
 
--- src/store/categories/categories.action.js
+++ src/store/categories/categories.action.js
@@ -1,6 +1,24 @@
 import { createAction } from "../../utils/reducer/createAction.utils";
 import { CATEGORIES_ACTION_TYPES } from "./categories.types";
+import {getCategoriesAndDocuments} from "../../utils/firebase/firebase.utils"
 
-export const setCategories = (categoriesArray) => {
-    return createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray)
+export const fetchCategoriesStart = () => 
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START)
+
+export const fetchCategoriesSuccess = (categoriesArray) => 
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categoriesArray)
+
+export const fetchCategoriesFailed = (error) =>
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error)
+
+export const fetchCategoriesStartAsync = () => async (dispatch) => {
+    dispatch(fetchCategoriesStart())
+
+    try {
+        const categoriesArray = await getCategoriesAndDocuments('categories')
+        dispatch(fetchCategoriesSuccess(categoriesArray))
+    }
+    catch (error) {
+        dispatch(fetchCategoriesFailed(error))
+    }
 }
